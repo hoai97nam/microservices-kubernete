23055a2f6ab35278157c7d56431cc8c2a95aecf7
nhnam <37695601+hoai97nam@users.noreply.github.com>
2023-06-19 22:22:41 +0700

    push to github

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=toliv22
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deploy project-ml-microservice-kubernetes --image="$dockerpath/project-ml:v1.0.0"
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment.apps/project-ml-microservice-kubernetes 8000:80
+
